7fa121c89795fbb375c9100cd5e1b5a436aefa3f
Elisabeth Kittle <elisabethkittle@gmail.com>
2021-06-14 16:21:55 -0400

    added basic functional component code to Home and Item

---
+++ src/components/Home.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import '../style/Home.css'
+
+const Home = () => {
+
+
+    return (
+        <div>
+            <div className="hero-container">
+                <h1>A gift for your least favorite victim of Thalassophobia</h1>
+
+            </div>
+
+
+        </div>
+
+    )
+}
+
+
+export default Home
